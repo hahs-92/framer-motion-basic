ff0f72b3501988ae4fadb87d44abe3d727f97042
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-09-30 17:40:08 -0500

    Drag Box2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 //componentes
 import Box1 from './components/Box1';
+import Box2 from './components/Box2'
 
 function App() {
   return (
     <div className="App">
       <Box1 />
+      <Box2 />
     </div>
   );
 }
---
+++ src/components/Box2.jsx
@@ -0,0 +1,27 @@
+import { motion } from 'framer-motion'
+
+const Box1 = () => {
+    return(
+        <article className='box-container'>
+            <motion.div 
+                className='box'
+                drag
+                dragConstraints={{
+                    top: 10,
+                    right: 20,
+                    bottom: 10,
+                    left: 0
+                }}
+                whileHover={{
+                    scale: 1.1
+                }}
+                whileTap={{
+                    scale: 0.9
+                }}
+            >
+            </motion.div>
+        </article>
+    )
+}
+
+export default Box1
