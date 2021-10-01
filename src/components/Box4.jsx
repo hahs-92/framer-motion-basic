1ad53c5df23a5efe9e38075b2dbace17d13a0a02
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-10-01 11:19:11 -0500

    keyframes

---
+++ src/components/Box4.jsx
@@ -0,0 +1,24 @@
+import { motion } from 'framer-motion'
+
+const Box4 = () => {
+
+    return(
+        <article className='box-container'>
+            <motion.div
+                className='box'
+                animate={{
+                    scale: [1, 1.4, 1.4, 1, 1],
+                    borderRadius: [ "20%", "20%", "50%", "50%", "20%"],
+                    rotate: [0, 0, 270, 270, 0 ]
+                }}
+                transition={{
+                    duration: 2
+                }}
+            >
+
+            </motion.div>
+        </article>
+    )
+}
+
+export default Box4
